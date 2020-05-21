bce1ee0c48b415fc410bd20964da6de30630ec2d
Marcos Ganine <marcosganine@gmail.com>
2020-05-21 20:04:18 -0300

    Remove fetch domain config logic

--- source/javascripts/remotelock-config.js
+++ source/javascripts/remotelock-config.js
@@ -1,29 +1,10 @@
 (function () {
   "use strict";
 
-  var CONFIG = {
-    "_default": {
-      apiBaseURL: "https://api.remotelock.com",
-      oAuthBaseURL: "https://connect.remotelock.com",
-      helpLinkURL: "https://support.remotelock.com",
-      zendeskWidgetKey: ""
-    },
-    "remotelock.com": {
-      apiBaseURL: "https://api.remotelock.com",
-      oAuthBaseURL: "https://connect.remotelock.com",
-      helpLinkURL: "https://support.remotelock.com",
-      zendeskWidgetKey: ""
-    }
+  window.config = {
+    apiBaseURL: "https://api.remotelock.com",
+    oAuthBaseURL: "https://connect.remotelock.com",
+    helpLinkURL: "https://support.remotelock.com",
+    zendeskWidgetKey: ""
   };
-
-  function fetchDomainConfig() {
-    for (var domain in CONFIG) {
-      if (document.location.hostname.endsWith(domain)) {
-        return CONFIG[domain];
-      }
-    }
-    return CONFIG['_default'];
-  };
-
-  window.config = fetchDomainConfig();
 })();
