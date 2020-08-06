c48c35e23ff7d9b9e910a190c2d31f4e0a2f5818
Marcos Ganine <marcosganine@gmail.com>
2020-08-06 20:05:54 -0300

    Change assets path configuration

--- config.rb
+++ config.rb
@@ -35,18 +35,16 @@ activate :autoprefixer do |config|
 end
 
 # Github pages require relative links
-activate :relative_assets
+# activate :relative_assets
 set :relative_links, true
 
 # Build Configuration
 configure :build do
-  activate :asset_hash, ignore: /remotelock-config.js/
+  activate :asset_hash, ignore: [/remotelock-config.js/, /images/]
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
-  # activate :relative_assets
-  # activate :asset_hash
   # activate :gzip
 end
 
