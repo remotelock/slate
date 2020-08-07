3bbd845c2b8775d056702f6a3a51f49db88571b4
Marcos Ganine <marcosganine@gmail.com>
2020-08-06 23:09:24 -0300

    Set assets base directory

--- config.rb
+++ config.rb
@@ -40,7 +40,8 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
-  activate :asset_hash, ignore: [/remotelock-config.js/, /images/]
+  activate :asset_host, host: '/api/docs/'
+  activate :asset_hash, ignore: [/remotelock-config.js/]
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
   activate :minify_css
